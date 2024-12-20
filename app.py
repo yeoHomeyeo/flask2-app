a9147da8d2adb4d9c6e51e5e2d4c3cf771be32df
Indigo_cE9 <chrisyeohc@outlook.com>
2024-12-21 00:27:42 +0800

    add python code

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, Jazeel!</p>"
+    return "<p>Hello, Babe!</p>"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
